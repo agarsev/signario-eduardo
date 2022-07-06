54445d5c9858df5210ba3cfb9eb20c92bf9d859c
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2022-07-06 13:39:18 +0200

    React + tailwind + parcel

---
+++ src/lista.js
@@ -0,0 +1,5 @@
+import ReactDOM from "react-dom";
+
+const App = () => <h1 class="bg-yellow-200">Hello, world!</h1>;
+
+ReactDOM.render(<App />, document.getElementById('app'));
--- src/main.js
+++ src/main.js
@@ -1,16 +1,8 @@
 const { app, BrowserWindow } = require('electron')
 
-const createWindow = () => {
-  const win = new BrowserWindow({
-    width: 800,
-    height: 600,
-  })
-
-  win.loadFile('index.html')
-}
-
 app.whenReady().then(() => {
-  createWindow()
+  const win = new BrowserWindow({});
+  win.loadFile('dist/lista.html')
 })
 
 app.on('window-all-closed', () => {
