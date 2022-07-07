fc8372195cc8d51e70d660457047b9a7b85acef9
Antonio F. G. Sevilla <antonio@garciasevilla.com>
2022-07-07 20:57:54 +0200

    Pick data path with dialog

--- src/main.js
+++ src/main.js
@@ -1,16 +1,27 @@
-const { app, BrowserWindow } = require('electron')
+const { app, BrowserWindow, dialog } = require('electron')
 const path = require('path');
 
-app.whenReady().then(() => {
+function MainWindow (dir) {
   const win = new BrowserWindow({
-    autoHideMenuBar: true,
+    //autoHideMenuBar: true,
     webPreferences: {
       preload: path.join(__dirname, 'preload.js'),
+      additionalArguments: [ dir ],
     },
   });
   win.loadFile('dist/index.html')
-})
+}
+
+function FolderPicker () {
+  return dialog.showOpenDialog({
+    properties: ['openDirectory'],
+  }).then(res => res.filePaths[0]);
+}
 
 app.on('window-all-closed', () => {
   if (process.platform !== 'darwin') app.quit()
 })
+
+app.whenReady()
+  .then(FolderPicker)
+  .then(MainWindow);
--- src/preload.js
+++ src/preload.js
@@ -1,9 +1,11 @@
 const { contextBridge } = require('electron');
 const { readdirSync } = require('fs');
 
+const data_dir = process.argv.pop();
+
 contextBridge.exposeInMainWorld('api', {
   list_videos: () => {
-    const videos = readdirSync('../data').map(f => {
+    const videos = readdirSync(data_dir).map(f => {
       const [date, num] = f.trim().split('_');
       return {date, num};
     });
